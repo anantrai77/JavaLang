f8c88aadeb5e06028dd378883eaf949ccd12a590
anantrai77 <anantraiom@gmail.com>
2025-12-19 00:00:06 +0530

    commit

---
+++ _068Practice.java
@@ -0,0 +1,8 @@
+public class _068Practice {
+    public static void main(String[] args) {
+        int [] marks = {12,34,56,78,90};
+        for(int i=0; i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
